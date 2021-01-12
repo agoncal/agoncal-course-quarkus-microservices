151362d4ed171393c009ec98b16f6e0e1b080b68
Antonio Goncalves <antonio.goncalves@gmail.com>
2021-01-12 15:29:35 +0100

    Adding parent pom

---
+++ bootstrap-rest-book.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+mvn -U io.quarkus:quarkus-maven-plugin:create \
+        -DprojectGroupId=org.agoncal.quarkus.microservices \
+        -DprojectArtifactId=rest-book \
+        -DclassName="org.agoncal.quarkus.microservices.book.BookResource" \
+        -Dpath="/api/books" \
+        -Dextensions="resteasy-jsonb"
---
+++ bootstrap-rest-number.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+mvn -U io.quarkus:quarkus-maven-plugin:create \
+        -DprojectGroupId=org.agoncal.quarkus.microservices \
+        -DprojectArtifactId=rest-number \
+        -DclassName="org.agoncal.quarkus.microservices.number.NumberResource" \
+        -Dpath="/api/number" \
+        -Dextensions="resteasy-jsonb"
